5525135a8bca2a19358674c5c6d44172e2f5ca98
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-16 01:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//complete this code
-// Rectangle class definition
 class Rectangle {
   constructor(width, height) {
     if (width <= 0 || height <= 0) {
@@ -9,32 +7,27 @@ class Rectangle {
     this._height = height;
   }
 
-  // Getter for width
   get width() {
     return this._width;
   }
 
-  // Getter for height
   get height() {
     return this._height;
   }
 
-  // Method to calculate area
   getArea() {
     return this._width * this._height;
   }
 }
 
-// Square class definition extending Rectangle
 class Square extends Rectangle {
   constructor(side) {
     if (side <= 0) {
       throw new Error("Side must be a positive integer.");
     }
-    super(side, side); // Call Rectangle constructor with equal width and height
+    super(side, side); 
   }
 
-  // Method to calculate perimeter of the square
   getPerimeter() {
     return 4 * this._width;
   }
