4b23ead6ce13165e8e66827d659399dfc99493f7
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-16 01:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width: number, height: number){
+		this.width = width;
+		this.height = height;
+	}
+	get ModifiedWidth(){
+		return this.width;
+	}
+	get ModifiedHeight(){
+		return this.height;
+	}
+	getArea(): number{
+		return this.width * this.height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	constructor(width: number, height: number,side: number){
+		super(width: number, height: number);
+		this.side = side;
+	}
+	
+	getPerimeter(): number{
+		return 4 * this.side;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
